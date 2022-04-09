613cbfe3661417e6e7efdb0a224c968265b655f8
jessiekil <kile984@newschool.edu>
2022-04-08 22:24:39 -0400

    ee

--- anime/happy.js
+++ anime/happy.js
@@ -1,78 +1,159 @@
 const db = {
-    id: 'appstQyf9n95NbHMt', 
-    // you can get this info on the api site. look for id
+    id: 'appstQyf9n95NbHMt',
     table: 'all',
-    // this is the name of your table collection 
     apiKey: 'keyyl7BUgBJhsZRv3'
-    // you can get this on your account
-};
-
-const airtableUrl = `https://api.airtable.com/v0/${db.id}/${db.table}?maxRecords=100&view=Grid%20view&api_key=${db.apiKey}`
-
-const fetchTitle = async () => {
+  };
+  
+  const airtableUrl =`https://api.airtable.com/v0/${db.id}/${db.table}?maxRecords=100&view=Grid%20view&api_key=${db.apiKey}`
+  
+  const slideshowContainer = document.getElementById('slideshow-container');
+  const prevButton = document.getElementById('prev');
+  const nextButton = document.getElementById('next');
+  
+  const fetchMovies = async () => {
     const response = await fetch(airtableUrl).then(data => data.json());
-    //console.log(response);
-
-    const myObject = {
-        title: 'title',
-        year: '1920',        
-    }
-
-    const myArray = ['title', 987098, 'hello'];
-    const isReleased = true;
-
-    // console.log(myObject.year)
-    // console.log(myArray[2]);
-
-    const container = document.getElementById('collection');
-
-// if I want to put 2, it will include hello. if i put 0, title
-    response.records.forEach((title) => {
-        //console.log(title);
-        // if the poster fields exists, do whatever inside the bracket for the sentence below
-        if (title.fields.poster) {
-            //console.log(title.fields.poster[0].url);
-            const posterImg = document.createElement('img');
-            // I CAN EITHER USE 밑에꺼 아님 그  밑에밑에 꺼 for image retrieving
-            posterImg.src = title.fields.poster[0].url;
-            // posterImg.setAttribute('src', title.fields.poster[0].url);
-            posterImg.classList.add('images');
-            container.append(posterImg);
-
+    // console.log(response);
+    buildSlideshow(response.records);
+    return response.records;
+  };
+  
+  const buildSlideshow = (movies) => {
+    let leftI = 0;
+    let rightI = 6;
+  
+    const articles = movies.slice(0, 7).map(buildSlide);
+    slideshowContainer.append(...articles);
+  
+    prevButton.addEventListener('click', () => {
+        leftI += 1;
+        rightI += 1;
+        if (rightI >= movies.length) {
+            rightI = 0;
         }
-// Append: whatever you put inside the function, you will add it onto the tool
-        if (title.fields.year) {
-           // console.log(title.fields.year);
+        if (leftI >= movies.length) {
+            leftI = 0;
         }
-// THIS IS WHERE YOU CAN INSERT TEXT
-        if(title.fields.medium) {
-            const mediumEl = document.createElement('p');
-            mediumEl.innerHTML = title.fields.medium;
-            container.append(mediumEl);
-        }
-
-        if(title.fields.year) {
-            const yearEl = document.createElement('p');
-            yearEl.innerHTML = title.fields.year;
-            yearEl.classList.add('year');
-            container.append(yearEl);
-        }
-
-        if(title.fields.emotions) {
-            const emotionsEl = document.createElement('p');
-            emotionsEl.innerHTML = title.fields.emotions;
-            emotionsEl.classList.add('emotions');
-            container.append(emotionsEl);
-
-            const {happyImgs} = data;
-            document.getElementById("happy").textContent = happyImgs;
-            console.log(happyImgs);
-        }
-        //console.log(title.fields.emotions);
-        // and so on. Also, P stands for paragraph for paragraph text elements. 
+        slideshowContainer.removeChild(slideshowContainer.children[0]);
+        slideshowContainer.append(buildSlide(movies[rightI]));
     });
-   
-};
-// classlist= is creating the CSS name
+  
+    nextButton.addEventListener('click', () => {
+        leftI -= 1;
+        rightI -= 1;
+        if (leftI < 0) {
+            leftI = movies.length - 1;
+        }
+        if (rightI < 0) {
+            rightI = movies.length - 1;
+        }
+        slideshowContainer.removeChild(slideshowContainer.querySelectorAll('article')[6]);
+        slideshowContainer.prepend(buildSlide(movies[leftI]));
+    });
+  };
+  
+  const buildSlide = (movie) => {
+    const movieContainer = document.createElement('article');
+    if (movie.fields.poster) {
+        // console.log(movie.fields.poster[0].url);
+        const posterImg = document.createElement('img');
+        posterImg.src = movie.fields.poster[0].url;
+        posterImg.classList.add('poster-img', 'dlkjfdl');
+        posterImg.id = 'poster-img-id';
+        movieContainer.append(posterImg);
+    }
+    if (movie.fields.release_date) {
+        // console.log(movie.fields.release_date);
+    }
+  
+    if (movie.fields.description) {
+        const descriptionEl = document.createElement('p');
+        descriptionEl.innerHTML = movie.fields.description;
+        descriptionEl.classList.add('movie-description');
+        movieContainer.append(descriptionEl);
+    }
+    return movieContainer;
+  };
+  
+  fetchMovies();
+  
+  
+  
+  
+  // const db = {
+//     id: 'appstQyf9n95NbHMt', 
+//     // you can get this info on the api site. look for id
+//     table: 'all',
+//     // this is the name of your table collection 
+//     apiKey: 'keyyl7BUgBJhsZRv3'
+//     // you can get this on your account
+// };
 
-fetchTitle();
+// const airtableUrl = `https://api.airtable.com/v0/${db.id}/${db.table}?maxRecords=100&view=Grid%20view&api_key=${db.apiKey}`
+
+// const fetchTitle = async () => {
+//     const response = await fetch(airtableUrl).then(data => data.json());
+//     //console.log(response);
+
+//     const myObject = {
+//         title: 'title',
+//         year: '1920',        
+//     }
+
+//     const myArray = ['title', 987098, 'hello'];
+//     const isReleased = true;
+
+//     // console.log(myObject.year)
+//     // console.log(myArray[2]);
+
+//     const container = document.getElementById('collection');
+
+// // if I want to put 2, it will include hello. if i put 0, title
+//     response.records.forEach((title) => {
+//         //console.log(title);
+//         // if the poster fields exists, do whatever inside the bracket for the sentence below
+//         if (title.fields.poster) {
+//             //console.log(title.fields.poster[0].url);
+//             const posterImg = document.createElement('img');
+//             // I CAN EITHER USE 밑에꺼 아님 그  밑에밑에 꺼 for image retrieving
+//             posterImg.src = title.fields.poster[0].url;
+//             // posterImg.setAttribute('src', title.fields.poster[0].url);
+//             posterImg.classList.add('images');
+//             container.append(posterImg);
+
+//         }
+// // Append: whatever you put inside the function, you will add it onto the tool
+//         if (title.fields.year) {
+//            // console.log(title.fields.year);
+//         }
+// // THIS IS WHERE YOU CAN INSERT TEXT
+//         if(title.fields.medium) {
+//             const mediumEl = document.createElement('p');
+//             mediumEl.innerHTML = title.fields.medium;
+//             container.append(mediumEl);
+//         }
+
+//         if(title.fields.year) {
+//             const yearEl = document.createElement('p');
+//             yearEl.innerHTML = title.fields.year;
+//             yearEl.classList.add('year');
+//             container.append(yearEl);
+//         }
+
+//         if(title.fields.emotions) {
+//             const emotionsEl = document.createElement('p');
+//             emotionsEl.innerHTML = title.fields.emotions;
+//             emotionsEl.classList.add('emotions');
+//             container.append(emotionsEl);
+
+//             const {happyImgs} = data;
+//             document.getElementById("happy").textContent = happyImgs;
+//             console.log(happyImgs);
+//         }
+//         //console.log(title.fields.emotions);
+//         // and so on. Also, P stands for paragraph for paragraph text elements. 
+//     });
+   
+// };
+// // classlist= is creating the CSS name
+
+// fetchTitle();
--- testing-anime/index.js
+++ testing-anime/index.js
@@ -1,93 +1,77 @@
 const db = {
-    id: 'appstQyf9n95NbHMt', 
-    // you can get this info on the api site. look for id
-    table: 'all',
-    // this is the name of your table collection 
-    apiKey: 'keyyl7BUgBJhsZRv3'
-    // you can get this on your account
+  id: 'appxfJxfQb92yyKkg',
+  table: 'movies',
+  apiKey: 'keykxG25CNr82Rf9Y'
 };
+
 const airtableUrl = `https://api.airtable.com/v0/${db.id}/${db.table}?maxRecords=100&view=Grid%20view&api_key=${db.apiKey}`
 
-
 const slideshowContainer = document.getElementById('slideshow-container');
-const fetchAnimes = async () => {
-    const response = await fetch(airtableUrl).then(data => data.json());
-    console.log(response);
-    const disgustEmotions = response.records.filter(emotion => {
-        return emotion.fields.emotions === "disgust";
-    });
-    console.log(disgustEmotions);
-    buildSlideshow(disgustEmotions);
-    return response.records;
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+const fetchMovies = async () => {
+  const response = await fetch(airtableUrl).then(data => data.json());
+  // console.log(response);
+  buildSlideshow(response.records);
+  return response.records;
 };
 
-const buildSlideshow = (animes) => {
-    let dataPos = -2;
-    animes.forEach((title) => {
-        console.log(title);
-        const item = document.createElement('li');
-        item.classList.add('carousel__item');
-        item.dataset.pos = `${dataPos}`;
-        dataPos += 1;
-        if (dataPos > 2) {
-            dataPos = 2;
-        }
-    
-        if (title.fields.poster) {
-            console.log(title.fields.poster[0].url);
-            const posterImg = document.createElement('img');
-            posterImg.src = title.fields.poster[0].url;
-            posterImg.classList.add('images');
-            item.append(posterImg);
-        }
-        slideshowContainer.append(item);
-    })
+const buildSlideshow = (movies) => {
+  let leftI = 0;
+  let rightI = 6;
 
-    const state = {};
-const carouselList = slideshowContainer;
-const carouselItems = document.querySelectorAll('.carousel__item');
-const elems = Array.from(carouselItems);
+  const articles = movies.slice(0, 7).map(buildSlide);
+  slideshowContainer.append(...articles);
 
-slideshowContainer.addEventListener('click', function (event) {
-  var newActive = event.target;
-  var isItem = newActive.closest('.carousel__item');
+  prevButton.addEventListener('click', () => {
+      leftI += 1;
+      rightI += 1;
+      if (rightI >= movies.length) {
+          rightI = 0;
+      }
+      if (leftI >= movies.length) {
+          leftI = 0;
+      }
+      slideshowContainer.removeChild(slideshowContainer.children[0]);
+      slideshowContainer.append(buildSlide(movies[rightI]));
+  });
 
-  if (!isItem || newActive.classList.contains('carousel__item_active')) {
-    return;
-  };
-  
-  update(newActive);
-});
-
-const update = function(newActive) {
-  const newActivePos = newActive.dataset.pos;
-
-  const current = elems.find((elem) => elem.dataset.pos == 0);
-  const prev = elems.find((elem) => elem.dataset.pos == -1);
-  const next = elems.find((elem) => elem.dataset.pos == 1);
-  const first = elems.find((elem) => elem.dataset.pos == -2);
-  const last = elems.find((elem) => elem.dataset.pos == 2);
-  
-  console.log(current);
-
-  current.classList.remove('carousel__item_active');
-  
-  [current, prev, next, first, last].forEach(item => {
-    var itemPos = item.dataset.pos;
-
-    item.dataset.pos = `${getPos(itemPos, newActivePos)}`
+  nextButton.addEventListener('click', () => {
+      leftI -= 1;
+      rightI -= 1;
+      if (leftI < 0) {
+          leftI = movies.length - 1;
+      }
+      if (rightI < 0) {
+          rightI = movies.length - 1;
+      }
+      slideshowContainer.removeChild(slideshowContainer.querySelectorAll('article')[6]);
+      slideshowContainer.prepend(buildSlide(movies[leftI]));
   });
 };
 
-const getPos = function (current, active) {
-  const diff = current - active;
-
-  if (Math.abs(current - active) > 2) {
-    return -current
+const buildSlide = (movie) => {
+  const movieContainer = document.createElement('article');
+  if (movie.fields.poster) {
+      // console.log(movie.fields.poster[0].url);
+      const posterImg = document.createElement('img');
+      posterImg.src = movie.fields.poster[0].url;
+      posterImg.classList.add('poster-img', 'dlkjfdl');
+      posterImg.id = 'poster-img-id';
+      movieContainer.append(posterImg);
+  }
+  if (movie.fields.release_date) {
+      // console.log(movie.fields.release_date);
   }
 
-  return diff;
-}
+  if (movie.fields.description) {
+      const descriptionEl = document.createElement('p');
+      descriptionEl.innerHTML = movie.fields.description;
+      descriptionEl.classList.add('movie-description');
+      movieContainer.append(descriptionEl);
+  }
+  return movieContainer;
 };
 
-fetchAnimes();
+fetchMovies();
